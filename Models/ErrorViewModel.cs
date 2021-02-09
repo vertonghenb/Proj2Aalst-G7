b435f336bbb63accf1d293f30af75c0ba559e357
Benjamin Vertonghen <benjamin.vertonghen@hogent.be>
2021-02-09 15:41:42 +0100

    Initial Scaffold

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Proj2Aalst_G7.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
